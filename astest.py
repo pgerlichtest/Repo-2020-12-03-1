6b9dc2d4af7c51192949b4689b798ee3584ef157
Paul Gerlich <paul@gerlich.io>
2021-07-15 15:41:39 -0500

    Update astest.py

--- astest.py
+++ astest.py
@@ -1,17 +1,1 @@
-MORE TESTING WITH ELON
-asd
-sad
-ad
-as
-d
-asd
-asd
-as
-da
-sd
-ad
-THAT Elon tho
-That one time when Elon was TESTing this code
-That one time when Elon was TESTing this code again
-That one time when Elon was TESTing this code again and again
-That one time when Elon was TESTing this code again and again ad
+i Elon TEST things
